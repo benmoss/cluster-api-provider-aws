3e3dcb76fd79a92f588a90b25cef155aca1ad05f
Ben Moss <benm@vmware.com>
2020-03-23 20:05:43 +0000

    Works

--- internal/subnet/subnet.go
+++ internal/subnet/subnet.go
@@ -3,48 +3,93 @@ package subnet
 import (
 	"math"
 	"net"
+	"strings"
 
 	"github.com/apparentlymart/go-cidr/cidr"
+	"github.com/pkg/errors"
 	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha3"
 )
+
+const maxCIDRMask = 28
+
+var errInvalidNetwork = errors.New("Could not find a valid subnet configuration")
 
 // If n=len(zones), divides the given CIDR into n+1 subnets, and then subdivides the
 // first subnet into n sub-subnets to create n public and n private subnets
 // Inspired by https://github.com/cloudposse/terraform-aws-dynamic-subnets
-func FromZones(networkCidr string, zones []string) (infrav1.Subnets, error) {
+func FromZones(existingSubnets []string, networkCidr string, zones []string) (infrav1.Subnets, error) {
 	_, network, err := net.ParseCIDR(networkCidr)
 	if err != nil {
 		return nil, err
 	}
-	var subnets infrav1.Subnets
-	publicSubnet, err := calculateSubnet(network, zones, 0)
-	if err != nil {
-		return nil, err
+	numZones := len(zones)
+	var existingNets []*net.IPNet
+	for _, s := range existingSubnets {
+		_, parsed, err := net.ParseCIDR(s)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse subnet %q", s)
+		}
+		existingNets = append(existingNets, parsed)
 	}
-	for i, zone := range zones {
-		public, err := calculateSubnet(publicSubnet, zones, i)
-		if err != nil {
-			return nil, err
-		}
-		private, err := calculateSubnet(network, zones, i+1)
-		if err != nil {
-			return nil, err
-		}
 
-		subnets = append(subnets, &infrav1.SubnetSpec{
-			IsPublic:         true,
-			CidrBlock:        public.String(),
-			AvailabilityZone: zone,
-		})
-		subnets = append(subnets, &infrav1.SubnetSpec{
-			IsPublic:         false,
-			CidrBlock:        private.String(),
-			AvailabilityZone: zone,
-		})
+CIDRs:
+	for i := 0; i < 100; i++ {
+	Offsets:
+		for j := 0; j < 100; j++ {
+			var result infrav1.Subnets
+			publicSubnet, err := calculateSubnet(network, numZones+i, j)
+			if err != nil {
+				if strings.HasPrefix(err.Error(), "prefix extension") {
+					continue CIDRs
+				}
+				return nil, err
+			}
+			if size, _ := publicSubnet.Mask.Size(); size >= maxCIDRMask {
+				return nil, errInvalidNetwork
+			}
+			newNets := append(existingNets, publicSubnet)
+			if err := cidr.VerifyNoOverlap(newNets, network); err != nil {
+				continue
+			}
+
+			for k, zone := range zones {
+				public, err := calculateSubnet(publicSubnet, numZones+i, k)
+				if err != nil {
+					return nil, err
+				}
+				if size, _ := public.Mask.Size(); size >= maxCIDRMask {
+					return nil, errInvalidNetwork
+				}
+				private, err := calculateSubnet(network, numZones+i, j+k+1)
+				if err != nil {
+					if strings.HasPrefix(err.Error(), "prefix extension") {
+						continue CIDRs
+					}
+					return nil, err
+				}
+				newNets := append(existingNets, public)
+				newNets = append(newNets, private)
+				if err := cidr.VerifyNoOverlap(newNets, network); err != nil {
+					continue Offsets
+				}
+
+				result = append(result, &infrav1.SubnetSpec{
+					IsPublic:         true,
+					CidrBlock:        public.String(),
+					AvailabilityZone: zone,
+				})
+				result = append(result, &infrav1.SubnetSpec{
+					IsPublic:         false,
+					CidrBlock:        private.String(),
+					AvailabilityZone: zone,
+				})
+			}
+			return result, nil
+		}
 	}
-	return subnets, nil
+	return nil, errors.New("Could not find a valid subnet configuration")
 }
 
-func calculateSubnet(network *net.IPNet, zones []string, index int) (*net.IPNet, error) {
-	return cidr.Subnet(network, int(math.Max(1.0, math.Ceil(math.Log2(float64(len(zones)))))), index)
+func calculateSubnet(network *net.IPNet, numZones int, index int) (*net.IPNet, error) {
+	return cidr.Subnet(network, int(math.Max(1.0, math.Ceil(math.Log2(float64(numZones))))), index)
 }
--- internal/subnet/subnet_test.go
+++ internal/subnet/subnet_test.go
@@ -2,9 +2,11 @@ package subnet
 
 import (
 	"encoding/json"
+	"net"
 	"reflect"
 	"testing"
 
+	"github.com/apparentlymart/go-cidr/cidr"
 	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha3"
 )
 
@@ -176,7 +178,7 @@ func TestFromZones(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			subnets, err := FromZones(tc.cidr, tc.zones)
+			subnets, err := FromZones(nil, tc.cidr, tc.zones)
 			if err != nil {
 				t.Errorf("failed to calculate subnets: %v", err)
 				return
@@ -198,6 +200,274 @@ func TestFromZones(t *testing.T) {
 					t.Errorf("Expected to find %s in %s", string(expected), string(actual))
 					return
 				}
+			}
+		})
+	}
+}
+
+func TestFromZonesSupportsExistingSubnets(t *testing.T) {
+	testCases := []struct {
+		name            string
+		zones           []string
+		cidr            string
+		existingSubnets []string
+		expected        infrav1.Subnets
+	}{
+		{
+			name:            "us-east-1 with standard default CIDR",
+			zones:           []string{"us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"},
+			cidr:            "10.0.0.0/16",
+			existingSubnets: []string{"10.0.0.0/24", "10.0.1.0/24"},
+			expected: infrav1.Subnets{
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.32.0/22",
+					AvailabilityZone: "us-east-1a",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.36.0/22",
+					AvailabilityZone: "us-east-1b",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.40.0/22",
+					AvailabilityZone: "us-east-1c",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.44.0/22",
+					AvailabilityZone: "us-east-1d",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.48.0/22",
+					AvailabilityZone: "us-east-1e",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.52.0/22",
+					AvailabilityZone: "us-east-1f",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.64.0/19",
+					AvailabilityZone: "us-east-1a",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.96.0/19",
+					AvailabilityZone: "us-east-1b",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.128.0/19",
+					AvailabilityZone: "us-east-1c",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.160.0/19",
+					AvailabilityZone: "us-east-1d",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.192.0/19",
+					AvailabilityZone: "us-east-1e",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.224.0/19",
+					AvailabilityZone: "us-east-1f",
+				},
+			},
+		},
+		{
+			name:            "smaller CIDR",
+			zones:           []string{"us-east-2a", "us-east-2b", "us-east-2c"},
+			existingSubnets: []string{"10.0.0.0/24", "10.0.1.0/24"},
+			cidr:            "10.0.0.0/20",
+			expected: infrav1.Subnets{
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.2.0/26",
+					AvailabilityZone: "us-east-2a",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.2.64/26",
+					AvailabilityZone: "us-east-2b",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.2.128/26",
+					AvailabilityZone: "us-east-2c",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.4.0/23",
+					AvailabilityZone: "us-east-2a",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.6.0/23",
+					AvailabilityZone: "us-east-2b",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.8.0/23",
+					AvailabilityZone: "us-east-2c",
+				},
+			},
+		},
+		{
+			name:            "one zone",
+			zones:           []string{"us-east-5a"},
+			existingSubnets: []string{"10.0.0.0/24", "10.0.1.0/24"},
+			cidr:            "10.0.0.0/16",
+			expected: infrav1.Subnets{
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.64.0/20",
+					AvailabilityZone: "us-east-5a",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.128.0/18",
+					AvailabilityZone: "us-east-5a",
+				},
+			},
+		},
+		{
+			name:            "large existing subnets leave little leftover",
+			zones:           []string{"us-east-2a", "us-east-2b", "us-east-2c"},
+			existingSubnets: []string{"10.0.0.0/17", "10.0.128.0/18"},
+			cidr:            "10.0.0.0/16",
+			expected: infrav1.Subnets{
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.192.0/24",
+					AvailabilityZone: "us-east-2a",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.193.0/24",
+					AvailabilityZone: "us-east-2b",
+				},
+				{
+					IsPublic:         true,
+					CidrBlock:        "10.0.194.0/24",
+					AvailabilityZone: "us-east-2c",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.208.0/20",
+					AvailabilityZone: "us-east-2a",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.224.0/20",
+					AvailabilityZone: "us-east-2b",
+				},
+				{
+					IsPublic:         false,
+					CidrBlock:        "10.0.240.0/20",
+					AvailabilityZone: "us-east-2c",
+				},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			subnets, err := FromZones(tc.existingSubnets, tc.cidr, tc.zones)
+			if err != nil {
+				t.Errorf("failed to calculate subnets: %v", err)
+				return
+			}
+			if len(tc.expected) != len(subnets) {
+				t.Errorf("expected to have %d subnets, got %d", len(tc.expected), len(subnets))
+				return
+			}
+			var allParsed []*net.IPNet
+			for _, exp := range tc.expected {
+				var found bool
+				for _, sn := range subnets {
+					if reflect.DeepEqual(exp, sn) {
+						found = true
+					}
+
+				}
+				if !found {
+					expected, _ := json.MarshalIndent(exp, "", "\t")
+					actual, _ := json.MarshalIndent(subnets, "", "\t")
+					t.Errorf("Expected to find %s in %s", string(expected), string(actual))
+					return
+				}
+				_, network, err := net.ParseCIDR(exp.CidrBlock)
+				if err != nil {
+					t.Errorf("failed to calculate subnets: %v", err)
+					return
+				}
+				allParsed = append(allParsed, network)
+			}
+			for _, s := range tc.existingSubnets {
+				_, network, err := net.ParseCIDR(s)
+				if err != nil {
+					t.Errorf("failed to calculate subnets: %v", err)
+					return
+				}
+				allParsed = append(allParsed, network)
+			}
+			_, super, err := net.ParseCIDR(tc.cidr)
+			if err != nil {
+				t.Errorf("failed to calculate subnets: %v", err)
+				return
+			}
+			if err := cidr.VerifyNoOverlap(allParsed, super); err != nil {
+				actual, _ := json.MarshalIndent(subnets, "", "\t")
+				t.Errorf("detected overlap in networks %v\n%s", err, string(actual))
+				return
+			}
+		})
+	}
+}
+
+func TestFromZonesErrors(t *testing.T) {
+	testCases := []struct {
+		name            string
+		zones           []string
+		cidr            string
+		existingSubnets []string
+	}{
+		{
+			name:            "all cidr space is used",
+			zones:           []string{"us-east-1a"},
+			cidr:            "10.0.0.0/16",
+			existingSubnets: []string{"10.0.0.0/17", "10.0.128.0/17"},
+		},
+		{
+			name:            "subnets are forced to be too small",
+			zones:           []string{"us-east-2a", "us-east-2b", "us-east-2c"},
+			existingSubnets: []string{"10.0.0.0/17", "10.0.128.0/18", "10.0.192.0/19", "10.0.224.0/20", "10.0.240.0/21"},
+			cidr:            "10.0.0.0/16",
+		},
+		{
+			name:            "too small cidr",
+			zones:           []string{"us-east-2a", "us-east-2b", "us-east-2c"},
+			existingSubnets: []string{},
+			cidr:            "10.0.0.0/27",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			subnets, err := FromZones(tc.existingSubnets, tc.cidr, tc.zones)
+			if err == nil {
+				actual, _ := json.MarshalIndent(subnets, "", "\t")
+				t.Errorf("expected test case to fail, but err is nil. got result:\n%s", actual)
+				return
+			}
+			if err.Error() != "Could not find a valid subnet configuration" {
+				t.Errorf("failed to calculate subnets: %v", err)
+				return
 			}
 		})
 	}
